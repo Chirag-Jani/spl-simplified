ed531ac653bc7fc875effc1ea5cc8e7fe08f5f7d
Chirag-Jani <>
2024-10-08 11:51:22 +0530

    refactor: customblock

--- src/simplespl.rs
+++ src/simplespl.rs
@@ -1,3 +1,18 @@
-pub fn test_function() -> core::result::Result<String> {
-    Ok("Ok".to_string())
+use anchor_lang::solana_program::account_info::AccountInfo;
+
+use anchor_lang::solana_program::program_pack::Pack;
+use anchor_lang::solana_program::pubkey::Pubkey;
+use anchor_lang::{context::CpiContext, Accounts};
+use anchor_lang::{solana_program, Result};
+use std::ops::Deref;
+
+pub use spl_token;
+pub use spl_token::ID;
+
+pub mod customblock {
+    use super::*;
+
+    pub fn amount() -> Result<u64> {
+        Ok(200)
+    }
 }
