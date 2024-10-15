8a2ed9fce94611813866f218df664e85948d2c33
Chirag-Jani <>
2024-10-15 18:07:57 +0530

    refactor: might work donno

--- src/simplespl.rs
+++ src/simplespl.rs
@@ -1,16 +1,16 @@
-use anchor_lang::prelude::{Program, Signer, System, UncheckedAccount};
+use anchor_lang::prelude::{Program, System, UncheckedAccount};
 use anchor_lang::solana_program::account_info::AccountInfo;
 use anchor_lang::solana_program::pubkey::Pubkey;
-use anchor_lang::Key;
 use anchor_lang::{context::CpiContext, Accounts};
 use anchor_lang::{solana_program, Result};
+use anchor_lang::{Key, ToAccountInfo};
 use mpl_token_metadata::types::DataV2;
 use solana_program::program::invoke_signed;
-use solana_program::rent::Rent;
-use solana_program::sysvar::Sysvar;
+// use solana_program::rent::Rent;
+// use solana_program::sysvar::Sysvar;
 pub use spl_token::ID;
 
-use crate::token::Token;
+use crate::metadata::{create_metadata_accounts_v3, CreateMetadataAccountsV3};
 
 pub fn mint_to<'info>(
     ctx: CpiContext<'_, '_, '_, 'info, MintTo<'info>>,
@@ -61,17 +61,13 @@ pub fn mint_simple<'info>(
 }
 
 pub fn metadata_thing<'info>(
-    _ctx: CpiContext<'_, '_, '_, 'info, MetaCtx<'info>>,
+    ctx: CpiContext<'_, '_, '_, 'info, MetaCtx<'info>>,
     token_name: String,
     token_symbol: String,
     token_uri: String,
     token_tax: u16,
 ) -> Result<()> {
-    let token_name_bytes = token_name.as_bytes();
-    let signer_seeds = &[token_name_bytes];
-    let signer = [&signer_seeds[..]];
-
-    let token_name: DataV2 = DataV2 {
+    let token_data: DataV2 = DataV2 {
         name: token_name.clone(),
         symbol: token_symbol,
         uri: token_uri,
@@ -80,6 +76,24 @@ pub fn metadata_thing<'info>(
         collection: None,
         uses: None,
     };
+
+    // Create the metadata accounts
+    let metadata_ctx = CpiContext::new_with_signer(
+        ctx.accounts.token_metadata_program,
+        CreateMetadataAccountsV3 {
+            payer: ctx.accounts.payer,
+            update_authority: ctx.accounts.update_authority,
+            mint: ctx.accounts.mint.clone(),
+            metadata: ctx.accounts.metadata.to_account_info(),
+            mint_authority: ctx.accounts.mint_authority,
+            system_program: ctx.accounts.system_program.to_account_info(),
+            rent: ctx.accounts.mint.clone().to_account_info(),
+        },
+        ctx.signer_seeds,
+    );
+
+    create_metadata_accounts_v3(metadata_ctx, token_data, false, true, None)?;
+
     Ok(())
 }
 
@@ -91,23 +105,11 @@ pub struct MintTo<'info> {
 }
 
 #[derive(Accounts)]
-#[instruction(token_name: String)]
 pub struct MetaCtx<'info> {
     pub token_metadata_program: AccountInfo<'info>,
     pub payer: AccountInfo<'info>,
     pub update_authority: AccountInfo<'info>,
-    #[account(
-        init,
-        seeds = [token_name.as_bytes()],
-        bump,
-        payer = caller,
-        mint::decimals = 6,
-        mint::authority = mint
-    )]
     pub mint: AccountInfo<'info>,
-    #[account(mut)]
-    pub caller: Signer<'info>,
-    pub token_program: Program<'info, Token>,
     pub metadata: UncheckedAccount<'info>,
     pub mint_authority: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
