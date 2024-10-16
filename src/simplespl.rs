089349a16af2b01b1d9dd44705e9a7e1f4c67a3d
Chirag-Jani <>
2024-10-16 16:14:21 +0530

    feat: moved metadata creation with mint

--- src/simplespl.rs
+++ src/simplespl.rs
@@ -1,9 +1,8 @@
-use anchor_lang::prelude::{Program, System, UncheckedAccount};
 use anchor_lang::solana_program::account_info::AccountInfo;
 use anchor_lang::solana_program::pubkey::Pubkey;
+use anchor_lang::Key;
 use anchor_lang::{context::CpiContext, Accounts};
 use anchor_lang::{solana_program, Result};
-use anchor_lang::{Key, ToAccountInfo};
 use mpl_token_metadata::types::DataV2;
 use solana_program::program::invoke_signed;
 pub use spl_token::ID;
@@ -31,13 +30,41 @@ pub fn mint_to<'info>(
 }
 
 pub fn mint_simple<'info>(
+    token_name: String,
+    token_symbol: String,
+    token_uri: String,
+    token_tax: u16,
+    payer: AccountInfo<'info>,
+    token_metadata_program: AccountInfo<'info>,
+    update_authority: AccountInfo<'info>,
+    metadata: AccountInfo<'info>,
+    mint_authority: AccountInfo<'info>,
+    system_program: AccountInfo<'info>,
+    rent: AccountInfo<'info>,
     token_program: AccountInfo<'info>,
+    ////
     mint: AccountInfo<'info>,
     to: AccountInfo<'info>,
     authority: AccountInfo<'info>,
     signer_seeds: &[&[u8]],
     amount: u64,
 ) -> Result<()> {
+    metadata_thing(
+        token_name.clone(),
+        token_symbol.clone(),
+        token_uri.clone(),
+        token_tax.clone(),
+        payer,
+        token_metadata_program,
+        update_authority,
+        mint.clone(),
+        metadata,
+        mint_authority,
+        system_program,
+        rent,
+        &[&signer_seeds],
+    )?;
+
     let ix = spl_token::instruction::mint_to(
         &spl_token::ID,
         &mint.key(),
@@ -58,12 +85,21 @@ pub fn mint_simple<'info>(
     Ok(())
 }
 
-pub fn metadata_thing<'info>(
-    ctx: CpiContext<'_, '_, '_, 'info, MetaCtx<'info>>,
+fn metadata_thing<'info>(
+    // ctx: CpiContext<'_, '_, '_, 'info, MetaCtx<'info>>,
     token_name: String,
     token_symbol: String,
     token_uri: String,
     token_tax: u16,
+    payer: AccountInfo<'info>,
+    token_metadata_program: AccountInfo<'info>,
+    update_authority: AccountInfo<'info>,
+    mint: AccountInfo<'info>,
+    metadata: AccountInfo<'info>,
+    mint_authority: AccountInfo<'info>,
+    system_program: AccountInfo<'info>,
+    rent: AccountInfo<'info>,
+    signer_seed: &[&[&[u8]]],
 ) -> Result<()> {
     let token_data: DataV2 = DataV2 {
         name: token_name.clone(),
@@ -77,17 +113,17 @@ pub fn metadata_thing<'info>(
 
     // Create the metadata accounts
     let metadata_ctx = CpiContext::new_with_signer(
-        ctx.accounts.token_metadata_program,
+        token_metadata_program,
         CreateMetadataAccountsV3 {
-            payer: ctx.accounts.payer,
-            update_authority: ctx.accounts.update_authority,
-            mint: ctx.accounts.mint.clone(),
-            metadata: ctx.accounts.metadata.to_account_info(),
-            mint_authority: ctx.accounts.mint_authority,
-            system_program: ctx.accounts.system_program.to_account_info(),
-            rent: ctx.accounts.mint.clone().to_account_info(),
+            payer,
+            update_authority,
+            mint,
+            metadata,
+            mint_authority,
+            system_program,
+            rent,
         },
-        ctx.signer_seeds,
+        signer_seed, // Keep the signer seeds from the original context
     );
 
     create_metadata_accounts_v3(metadata_ctx, token_data, false, true, None)?;
@@ -100,16 +136,4 @@ pub struct MintTo<'info> {
     pub mint: AccountInfo<'info>,
     pub to: AccountInfo<'info>,
     pub authority: AccountInfo<'info>,
-}
-
-#[derive(Accounts)]
-pub struct MetaCtx<'info> {
-    pub token_metadata_program: AccountInfo<'info>,
-    pub payer: AccountInfo<'info>,
-    pub update_authority: AccountInfo<'info>,
-    pub mint: AccountInfo<'info>,
-    pub metadata: UncheckedAccount<'info>,
-    pub mint_authority: AccountInfo<'info>,
-    pub system_program: Program<'info, System>,
-    // pub rent: Sysvar<'info, Rent>,
 }
